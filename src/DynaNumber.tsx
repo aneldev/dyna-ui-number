1c7e04f91a6bfe25475583be877a1c2bfd3f83e9
Dennis Teo <dynasdaq@gmail.com>
2018-03-08 07:09:43 +0100

    1st commit

---
+++ src/DynaNumber.tsx
@@ -0,0 +1,121 @@
+import * as React from "react";
+import {DynaFieldWrapper, EMode, EColor, EStyle, ESize} from "dyna-ui-field-wrapper"
+import {isNumber, round, roundToString} from "dyna-loops";
+import {faIcon} from "./utils/faIcon";
+
+import "./layout.less";
+import "./color.less";
+
+export {EColor, EStyle}
+
+export interface IDynaNumberProps {
+  style?: EStyle;
+  mode?: EMode;
+  color?: EColor;
+  size?: ESize;
+  name?: string;
+  label?: TContent;
+  required?: TContent;
+  min?: number;
+  max?: number;
+  value: number;
+  step?: number;
+  precision?: number; // decimal
+  validationMessage?: TContent;
+  footer?: TContent;
+  inputProps?: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+  onChange?: (name: string, value: number) => void;
+}
+
+export type TContent = string | JSX.Element;
+
+export class DynaNumber extends React.Component<IDynaNumberProps> {
+  static defaultProps: IDynaNumberProps = {
+    mode: EMode.EDIT,
+    style: EStyle.INLINE_ROUNDED,
+    color: EColor.WHITE_BLACK,
+    size: ESize.MEDIUM,
+    name: '',
+    label: null,
+    min: null,
+    max: null,
+    value: 0,
+    step: 1,
+    precision: 0,
+    validationMessage: null,
+    inputProps: {},
+    footer: null,
+    onChange: (name: string, value: number) => undefined,
+  };
+
+  private handleChange(value: string) {
+    const {name, precision, min, max, onChange} = this.props;
+    if (!isNumber(value)) return;
+    const n: number = round(Number(value), precision);
+    if (min != null && n < min) return;
+    if (max != null && n > max) return;
+    onChange && onChange(name, n);
+  }
+
+  private handleButtonValueClick(factor: number): void {
+    const {name, step, value, min, max, onChange} = this.props;
+    const newValue: number = value + (factor * step);
+    if (min != null && newValue < min) return;
+    if (max != null && newValue > max) return;
+    onChange(name, newValue);
+  }
+
+  private renderValueButton(factor: number): JSX.Element {
+    const icon: string = factor === +1 ? 'plus-circle' : 'minus-circle';
+
+    return (
+      <div
+        className="dn-number--value-button"
+        onClick={this.handleButtonValueClick.bind(this, factor)}
+      >
+        <div className="icon-container">{faIcon(icon)}</div>
+      </div>
+
+    );
+  }
+
+  private getFormatedValue(): string {
+    const {value, precision} = this.props;
+    return roundToString(value, precision);
+  }
+
+  public render(): JSX.Element {
+    const {
+      style, color, mode, size,
+      label, required,
+      inputProps,
+      validationMessage, footer,
+    } = this.props;
+
+    return (
+      <DynaFieldWrapper
+        className="dyna-number"
+        mode={mode}
+        style={style}
+        color={color}
+        size={size}
+        inputElementSelector="input"
+        label={label}
+        required={required}
+        validationMessage={validationMessage}
+        footer={footer}
+      >
+        <div className="dn-number--input-content">
+          {this.renderValueButton(+1)}
+          <input
+            readOnly
+            value={this.getFormatedValue()}
+            {...inputProps}
+            onChange={e => this.handleChange(e.target.value)}
+          />
+          {this.renderValueButton(-1)}
+        </div>
+      </DynaFieldWrapper>
+    );
+  }
+}
---
+++ tests/unit/main.test.ts
@@ -0,0 +1,13 @@
+declare let window: any;
+declare let global: any, jasmine: any, describe: any, clearTest: any, it: any, expect: any;
+if (typeof jasmine !== 'undefined') jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+import {DynaNumber} from '../../src';
+
+// help: https://facebook.github.io/jest/docs/expect.html
+
+describe('Simple unit test', () => {
+  it('should import the component', () => {
+		expect(DynaNumber).not.toBe(undefined);
+	});
+});
